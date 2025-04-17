b99e6b3662e8e94b37a41a5904d10af585f83e31
achristoforou1 <a_christoforou1@yahoo.com>
2025-04-17 13:16:13 +0300

    created event listeners and test for errors

--- scripts/game.js
+++ scripts/game.js
@@ -14,7 +14,7 @@ function newGame() {
     for (let circle of document.getElementsByClassName("circle")) {
         if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
-                if (game.currentGame.length > 0) {
+                if (game.currentGame.length > 0 && !game.turnInProgress) {
                     let move = e.target.getAttribute("id");
                     game.lastButton = move;
                     lightsOn(move);
@@ -37,12 +37,14 @@ function addTurn() {
 }
 
 function showTurns() {
+    game.turnInProgress = true;
     game.turnNumber = 0;
     let turns = setInterval(function () {
         lightsOn(game.currentGame[game.turnNumber]);
         game.turnNumber++;
         if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
+            game.turnInProgress = false;
         }
     }, 800);
 }
